09a5b5970b04041ff9e53858f834884bf686bdbe
vincent0628 <vincent0910628007@gmail.com>
2020-10-14 10:00:33 +0800

    add datetime

--- webcam_log.py
+++ webcam_log.py
@@ -5,17 +5,16 @@ Created on Sun May  3 18:33:27 2020
 @author: vincent
 """
 import cv2
-import time
+import datetime
 global t
 
-def save_log(status):
+def save_log(time, status,avg=None):
     global t
-    import time
     import pandas as pd
-    time = time.strftime("%Y/%m/%d %H:%M:%S")
     content = {
         'date': [time],
         'option': [status],
+        'pixel_value': [avg],
     }
     if status == 'dark':
         t = time+' dark'
@@ -28,39 +27,43 @@ def main():
     cap = cv2.VideoCapture(0)
     status = 'open'
     flag = True
+    save_log(datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"), status)
     while(True):
-      ret, frame = cap.read()
-      frame = cv2.flip(frame, -1) #縱向翻轉
-      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-         
-      # The range for a pixel's value in grayscale is (0-255), 127 lies midway
-      if round(cv2.mean(gray)[0]) < 10:
-          if not status == 'dark':
-              flag = True
-          status = 'dark'
-
-      else:
-          if not status == 'bright':
-              flag = True
-          status = 'bright'
-    
-      if flag == True:
-          save_log(status)
-          flag = False
-          
-      if status == 'dark':
-          cv2.putText(frame, t, (10, 280), 
-                      cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, 
-                      (0, 255, 255), 1, cv2.LINE_AA)
-      frame = cv2.resize(frame, (720, 480))
-      cv2.putText(frame, time.strftime("%Y/%m/%d %H:%M:%S"), (350, 50), 
-                  cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, 
-                  (255, 255, 255), 1, cv2.LINE_AA)
-      cv2.imshow('frame', frame)
-    
-      if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-    
+        now = datetime.datetime.now()
+        time = now.strftime("%Y/%m/%d %H:%M:%S")
+        ret, frame = cap.read()
+        frame = cv2.flip(frame, -1) #縱向翻轉
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # 平時avg為70
+        avg = round(cv2.mean(gray)[0])
+        # print(avg)
+        if avg < 5:
+            if not status == 'dark': flag = True
+            status = 'dark'
+        elif 55> avg > 10:
+            if not status == 'bright': flag = True
+            status = 'bright'
+        
+        elif avg > 60:
+            if not status == 'light': flag = True
+            status = 'light'
+            
+        if flag == True:
+            save_log(time, status,avg)
+            flag = False
+              
+        if status == 'dark':
+            cv2.putText(frame, t, (10, 280), 
+                        cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, 
+                        (0, 255, 255), 1, cv2.LINE_AA)
+        frame = cv2.resize(frame, (720, 480))
+        cv2.putText(frame, time , (350, 50), 
+                    cv2.FONT_HERSHEY_SCRIPT_SIMPLEX, 1, 
+                    (255, 255, 255), 1, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+          break
     cap.release()
     cv2.destroyAllWindows()
 
